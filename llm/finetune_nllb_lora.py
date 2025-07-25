864e5d5a85516826926416df7b09ad0b53939a7d
faridig <faridigouti@gmail.com>
2025-07-26 01:51:00 +0200

    chgt llm

--- llm/finetune_nllb_lora.py
+++ llm/finetune_nllb_lora.py
@@ -106,7 +106,7 @@ def main():
         per_device_train_batch_size=8,
         learning_rate=5e-4,
         # MODIFICATION : Augmentation du nombre d'époques à 5
-        num_train_epochs=0.03,
+        num_train_epochs=0.5,
         fp16=True,
         logging_dir="./logs",
         save_strategy="steps",
